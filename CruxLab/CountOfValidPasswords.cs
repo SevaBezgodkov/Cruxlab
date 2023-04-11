0d49e4afe29c08cf81c1d021f53550dac3c4686f
SevaBezgodkov <87025997+SevaBezgodkov@users.noreply.github.com>
2023-04-11 10:01:41 +0300

    Refactoring

---
+++ CruxLab/CountOfValidPasswords.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CruxLab
+{
+    public class CountOfValidPasswords
+    {
+        public static void Counter()
+        {
+            string file = "C:\\CruxLab.txt";
+            string[] fileLines = File.ReadAllLines(file);
+            int validCount = 0;
+            foreach (string line in fileLines)
+            {
+                string[] lineParts = line.Split(':');
+                string[] requirementsSymbols = lineParts[0].Split(' ');
+                string[] rangeParts = requirementsSymbols[1].Split('-');
+                char requiredChar = requirementsSymbols[0][0];
+                int minCount = int.Parse(rangeParts[0]);
+                int maxCount = int.Parse(rangeParts[1]);
+                string password = lineParts[1].Trim();
+                int count = password.Count(c => c == requiredChar);
+                if (count >= minCount && count <= maxCount)
+                {
+                    validCount++;
+                }
+            }
+            Console.WriteLine("Valid passwords: " + validCount);
+        }
+    }
+}
--- CruxLab/Program.cs
+++ CruxLab/Program.cs
@@ -1,25 +1,9 @@
-﻿internal class Program
+﻿using CruxLab;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        string file = "./CruxLab.txt";
-        string[] fileLines = File.ReadAllLines(file);
-        int validCount = 0;
-        foreach (string line in fileLines)
-        {
-            string[] lineParts = line.Split(':');
-            string[] requirementsSymbols = lineParts[0].Split(' ');
-            string[] rangeParts = requirementsSymbols[1].Split('-');
-            char requiredChar = requirementsSymbols[0][0];
-            int minCount = int.Parse(rangeParts[0]);
-            int maxCount = int.Parse(rangeParts[1]);
-            string password = lineParts[1].Trim();
-            int count = password.Count(c => c == requiredChar);
-            if (count >= minCount && count <= maxCount)
-            {
-                validCount++;
-            }
-        }
-        Console.WriteLine("Valid passwords: " + validCount);
+        CountOfValidPasswords.Counter();
     }
 }
